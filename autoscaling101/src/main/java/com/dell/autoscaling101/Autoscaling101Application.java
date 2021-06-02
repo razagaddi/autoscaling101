c799a42275f6ea7511ee34d59c700e1b446a60f8
razagaddi <razagaddi@users.noreply.github.com>
2021-06-02 10:50:35 -0500

    init

---
+++ autoscaling101/src/main/java/com/dell/autoscaling101/Autoscaling101Application.java
@@ -0,0 +1,13 @@
+package com.dell.autoscaling101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autoscaling101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Autoscaling101Application.class, args);
+	}
+
+}
